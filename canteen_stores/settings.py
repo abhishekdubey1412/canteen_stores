ba6c96a72769a7b518c3ff5f95ddb9860f3eadbd
Abhishek Dubey <49669176+abhishekdubey1412@users.noreply.github.com>
2024-02-29 06:50:37 +0000

    modified setting file

--- canteen_stores/settings.py
+++ canteen_stores/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0k314&bo(dg%nu4il$*&8&+e&v0ptczhgqm$)iqc2m(f6du0pf'
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
